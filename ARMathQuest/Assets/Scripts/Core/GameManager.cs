29dcaabf13a982b680bd0d831ee62ec42cd25e79
Kevin Gomez <123139448+Nightsu32@users.noreply.github.com>
2026-03-02 19:47:30 -0800

    updated game manager (Press 0, 1, or 2 to simulate selecting an answer)

--- ARMathQuest/Assets/Scripts/Core/GameManager.cs
+++ ARMathQuest/Assets/Scripts/Core/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     private QuestionGenerator questionGenerator;
+    private int currentCorrectIndex;
 
     private void Start()
     {
@@ -11,29 +12,48 @@ public class GameManager : MonoBehaviour
         GenerateNewQuestion();
     }
 
+    private void Update()
+    {
+        // Press 0, 1, or 2 to simulate selecting an answer
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            CheckAnswer(0);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            CheckAnswer(1);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            CheckAnswer(2);
+    }
+
     public void GenerateNewQuestion()
     {
         var questionData = questionGenerator.GenerateQuestion();
 
+        currentCorrectIndex = questionData.correctIndex;
+
+        Debug.Log("----- NEW QUESTION -----");
         Debug.Log(questionData.questionText);
 
         for (int i = 0; i < questionData.answers.Count; i++)
         {
             Debug.Log($"Option {i}: {questionData.answers[i]}");
         }
-
-        CheckAnswer(0, questionData.correctIndex);
     }
 
-    public void CheckAnswer(int selectedIndex, int correctIndex)
+    public void CheckAnswer(int selectedIndex)
     {
-        if (selectedIndex == correctIndex)
+        if (selectedIndex == currentCorrectIndex)
         {
+            Debug.Log("Player selected correct answer.");
             ScoreManager.Instance.RegisterCorrect();
         }
         else
         {
+            Debug.Log("Player selected wrong answer.");
             ScoreManager.Instance.RegisterIncorrect();
         }
+
+        // Generate a new question after answering
+        GenerateNewQuestion();
     }
 }
