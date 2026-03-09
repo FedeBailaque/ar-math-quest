c2d725ad417a633e77f12726b7c821f2913b5221
Firae <owen.scott.181@my.csun.edu>
2026-03-09 16:23:11 -0700

    Updated depreciated features

--- ARMathQuest/Assets/Scripts/PlaneDetectionLogger.cs
+++ ARMathQuest/Assets/Scripts/PlaneDetectionLogger.cs
@@ -8,10 +8,10 @@ public class PlaneDetectionLogger : MonoBehaviour
 
     void Awake() => _planeManager = GetComponent<ARPlaneManager>();
 
-    void OnEnable() => _planeManager.planesChanged += OnPlanesChanged;
-    void OnDisable() => _planeManager.planesChanged -= OnPlanesChanged;
+    void OnEnable() => _planeManager.trackablesChanged.AddListener(OnPlanesChanged);
+    void OnDisable() => _planeManager.trackablesChanged.RemoveListener(OnPlanesChanged);
 
-    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    private void OnPlanesChanged(ARTrackablesChangedEventArgs<ARPlane> args)
     {
         Debug.Log($"Planes: added={args.added.Count}, updated={args.updated.Count}, removed={args.removed.Count}, total={_planeManager.trackables.count}");
     }
